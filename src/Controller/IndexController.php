1ad38a0c2b8bd7ec96e8acfd4e89619d7e14db99
zhongsheng <tianzhongsheng168@gmail.com>
2021-12-20 18:12:40 +0800

    更新主页

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -13,6 +13,7 @@ class IndexController extends AbstractController
     {
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
+            'time'            => (strtotime("2021-12-27 00:00:00") - time())*1000,
         ]);
     }
 }
