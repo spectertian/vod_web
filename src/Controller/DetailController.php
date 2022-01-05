77fad6fde4eebcc871547a42db4204e67e9d1355
zhongsheng <tianzhongsheng168@gmail.com>
2022-01-05 18:11:00 +0800

    调整样式

--- src/Controller/DetailController.php
+++ src/Controller/DetailController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\DocumentRepository\ListsRepository;
-use MongoDB\BSON\ObjectId;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
