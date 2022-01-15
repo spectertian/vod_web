a711b0806e1cf9022ebbf9068e7fdc47a15df777
zhongsheng <tianzhongsheng168@gmail.com>
2022-01-15 08:25:01 +0800

    搜索

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use App\DocumentRepository\ListsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SearchController extends AbstractController
 {
     #[Route('/search', name: 'search')]
-    public function index(): Response
+    public function index(ListsRepository $listsRepository, Request $request): Response
     {
+        $keyword        = $request->get("keyword");
+        $submittedToken = $request->request->get('_token');
+        $res = $listsRepository->findBy(["title"]);
+        if ($this->isCsrfTokenValid('search-item', $submittedToken)) {
+
+
+        } else {
+//            return $this->redirectToRoute('search', ["keyword" => $keyword]);
+        }
+
+
         return $this->render('search/index.html.twig', [
-            'controller_name' => 'SearchController',
+            'res' => $res,
         ]);
     }
 }
--- src/Form/MessageType.php
+++ src/Form/MessageType.php
@@ -42,8 +42,14 @@ class MessageType extends AbstractType
     {
         $resolver->setDefaults([
             // Configure your form options here
-            'data-class'    => Message::class,
-            'csrf_token_id' => 'task_item',
+            'data-class'      => Message::class,
+            // enable/disable CSRF protection for this form
+            'csrf_protection' => true,
+            // the name of the hidden HTML field that stores the token
+            'csrf_field_name' => '_token',
+            // an arbitrary string used to generate the value of the token
+            // using a different string for each form improves its security
+            'csrf_token_id'   => 'task_item',
 
         ]);
     }
