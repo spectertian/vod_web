e96e474577e49b7b37d4920a1ada3b153248498f
zhongsheng <tianzhongsheng168@gmail.com>
2022-01-17 16:31:40 +0800

    分页

--- src/Controller/RetrieveController.php
+++ src/Controller/RetrieveController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\DocumentRepository\ListsRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,10 +12,37 @@ use Symfony\Component\Routing\Annotation\Route;
 class RetrieveController extends AbstractController
 {
     #[Route('/retrieve', name: 'retrieve')]
-    public function index(ListsRepository $listsRepository, Request $request): Response
+    public function index(ListsRepository $listsRepository, Request $request, PaginatorInterface $paginator): Response
     {
+        $query      = $listsRepository->createQueryBuilder();
+        $pagination = $paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+            10
+        );
+
+//        dump($pagination);
         return $this->render('retrieve/index.html.twig', [
-            'title' => '索引',
+            'title'      => '索引',
+            'pagination' => $pagination,
         ]);
     }
+
+    #[Route('/retrieve_category', name: 'retrieve_category')]
+    public function category(ListsRepository $listsRepository, Request $request, PaginatorInterface $paginator): Response
+    {
+        $query      = $listsRepository->createQueryBuilder();
+        $pagination = $paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+            10
+        );
+
+        return $this->render('news/index.html.twig', [
+            'title'      => '最新电影',
+            'pagination' => $pagination
+        ]);
+    }
+
+
 }
