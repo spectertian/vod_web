94dd088501ee51d70530e5ca94724dafb4aca8ed
zhongsheng <tianzhongsheng168@gmail.com>
2022-01-18 14:31:24 +0800

    检索

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\DocumentRepository\ListsRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,22 +12,28 @@ use Symfony\Component\Routing\Annotation\Route;
 class SearchController extends AbstractController
 {
     #[Route('/search', name: 'search')]
-    public function index(ListsRepository $listsRepository, Request $request): Response
+    public function index(ListsRepository $listsRepository, Request $request, PaginatorInterface $paginator): Response
     {
-        $keyword        = $request->get("keyword");
-        $submittedToken = $request->request->get('_token');
-        $res            = $listsRepository->findBy(["title"]);
-        if ($this->isCsrfTokenValid('search-item', $submittedToken)) {
+        $keyword = $request->get("keyword");
+        $query   = $listsRepository->createQueryBuilder();
+//        $query->field('type')->equals($type);
 
+        $query->addOr($query->expr()->field('type')->equals(new \MongoRegex('/.*' . $keyword . '.*/i')));
+//        $query->addOr($query->expr()->field('tags')->equals(new \MongoRegex('/.*' . $keyword . '.*/i')));
+//        $query->addOr($query->expr()->field('stars')->equals(new \MongoRegex('/.*' . $keyword . '.*/i')));
+//        $query->addOr($query->expr()->field('director')->equals(new \MongoRegex('/.*' . $keyword . '.*/i')));
+//        $query->addOr($query->expr()->field('title')->equals(new \MongoRegex('/.*' . $keyword . '.*/i')));
+//        $query->addOr($query->expr()->field('long_title')->equals(new \MongoRegex('/.*' . $keyword . '.*/i')));
+        $pagination = $paginator->paginate(
+            $query, /* query NOT result */
+            $request->query->getInt('page', 1), 10);
 
-        } else {
-//            return $this->redirectToRoute('search', ["keyword" => $keyword]);
-        }
-
+//        dump($pagination);
 
         return $this->render('search/index.html.twig', [
-            'res'   => $res,
-            'title' => '搜索结果',
+            'pagination' => $pagination,
+            'title'      => '搜索结果',
+            'keyword'    => $keyword,
         ]);
     }
 }
