c9c0516cd85b1793a3db2a7deed245fd58503502
zhongsheng <tianzhongsheng168@gmail.com>
2022-01-21 22:52:56 +0800

    排序

--- src/Controller/TopicController.php
+++ src/Controller/TopicController.php
@@ -15,12 +15,13 @@ class TopicController extends AbstractController
     #[Route('/topic.html', name: 'topic')]
     public function index(TopicRepository $topicRepository, PaginatorInterface $paginator, Request $request): Response
     {
-        $query      = $topicRepository->createQueryBuilder();
+        $query = $topicRepository->createQueryBuilder();
+        $query->sort(['date' => 'desc']);
         $pagination = $paginator->paginate(
             $query, /* query NOT result */
             $request->query->getInt('page', 1), 20);
 
-        $hotList = $topicRepository->findBy([], ['n_id' => 'desc'], 9, 3);
+        $hotList = $topicRepository->findBy([], ['data' => 'desc'], 9, 3);
         return $this->render('topic/index.html.twig', [
             'title'      => '专题',
             'pagination' => $pagination,
