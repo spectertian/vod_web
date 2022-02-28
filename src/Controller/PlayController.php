0c2aee0b804cfefb0f3ee22284965e7ff6480664
zhongsheng <tianzhongsheng168@gmail.com>
2022-03-01 00:27:50 +0800

    兼容手机

--- src/Controller/PlayController.php
+++ src/Controller/PlayController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\DocumentRepository\VodListRepository;
 use App\Service\RecommendList;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Mobile_Detect;
