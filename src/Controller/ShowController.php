bcb8e31a604367ecdc63887ca87a8ab51d53f912
zhongsheng <tianzhongsheng168@gmail.com>
2022-02-28 21:45:50 +0800

    兼容手机

--- src/Controller/ShowController.php
+++ src/Controller/ShowController.php
@@ -9,11 +9,12 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Yaml\Yaml;
+use Mobile_Detect;
 
 class ShowController extends AbstractController
 {
     #[Route('/show_category/{type}-{tag}-{area}-{year}.html', name: 'show_category', defaults: ['type' => "1", 'tag' => 0, 'area' => 0, 'year' => 0])]
-    public function category(VodListRepository $vodListRepository, Request $request, PaginatorInterface $paginator, $type, $tag, $area, $year): Response
+    public function category(VodListRepository $vodListRepository, Request $request, PaginatorInterface $paginator, Mobile_Detect $mobile_Detect, $type, $tag, $area, $year): Response
     {
         $query = $vodListRepository->createQueryBuilder();
         if ($type) {
@@ -34,17 +35,26 @@ class ShowController extends AbstractController
 
         $typeList = Yaml::parseFile(dirname(__DIR__) . '/../config/web_tag.yaml');
 
+
+        if ($mobile_Detect->isMobile()) {
+            $tmp = 'm/show/index.html.twig';
+            $pg  = 24;
+
+        } else {
+            $tmp = 'show/index.html.twig';
+            $pg  = 32;
+        }
+
+
         $pagination = $paginator->paginate(
             $query,
             $request->query->getInt('page', 1),
-            32
+            $pg
         );
-
 
         $pagination->setTemplate('show/pagination.html.twig');
 
-
-        return $this->render('show/index.html.twig', [
+        return $this->render($tmp, [
             'pagination' => $pagination,
             'typeList'   => $typeList,
             'tag'        => $tag,
