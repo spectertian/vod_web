e1d1fd753b4bce62cb3189865e2a6360077b3abb
zhongsheng <tianzhongsheng168@gmail.com>
2022-02-28 23:51:23 +0800

    兼容手机

--- src/Controller/PlayController.php
+++ src/Controller/PlayController.php
@@ -8,15 +8,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Mobile_Detect;
 
 class PlayController extends AbstractController
 {
     #[Route('/play/{s_id}/{p_id}', name: 'play')]
-    public function index(VodListRepository $vodListRepository, RecommendList $recommendList, $s_id, $p_id): Response
+    public function index(VodListRepository $vodListRepository, RecommendList $recommendList, Mobile_Detect $mobile_Detect, $s_id, $p_id): Response
     {
         $info     = $vodListRepository->find($s_id);
         $play_url = $info->getPlay()[1]['list'][$p_id]['url'];
-        return $this->render('play/index.html.twig', [
+        if ($mobile_Detect->isMobile()) {
+            $tmp = 'm/play/index.html.twig';
+        } else {
+            $tmp = 'play/index.html.twig';
+        }
+        return $this->render($tmp, [
             'info'           => $info,
             'recommend_list' => $recommendList->getHotByTypeId($info->getTypeId()),
             'topic'          => $recommendList->getHotByTypeName($info->getType()),
