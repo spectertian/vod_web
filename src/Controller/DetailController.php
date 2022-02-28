6c3a8732c248b45537eacb1f61192f282f8c31c2
zhongsheng <tianzhongsheng168@gmail.com>
2022-02-28 23:16:27 +0800

    兼容手机

--- src/Controller/DetailController.php
+++ src/Controller/DetailController.php
@@ -2,23 +2,29 @@
 
 namespace App\Controller;
 
-use App\DocumentRepository\ListsRepository;
 use App\DocumentRepository\VodListRepository;
 use App\Service\RecommendList;
-use App\Service\VodService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Mobile_Detect;
 
 class DetailController extends AbstractController
 {
     #[Route('/detail/{id}.html', name: 'detail', methods: ['GET'])]
-    public function index(VodListRepository $vodListRepository, Request $request, RecommendList $recommendList): Response
+    public function index(VodListRepository $vodListRepository, Request $request, RecommendList $recommendList, Mobile_Detect $mobile_Detect): Response
     {
         $id   = $request->get('id');
         $info = $vodListRepository->find($id);
-        return $this->render('detail/index.html.twig', [
+
+        if ($mobile_Detect->isMobile()) {
+            $tmp = 'm/detail/index.html.twig';
+        } else {
+            $tmp = 'detail/index.html.twig';
+        }
+
+        return $this->render($tmp, [
             'info'           => $info,
             'topic'          => $recommendList->getHotByTypeName($info->getType()),
             'recommend_list' => $recommendList->getHotByTypeId($info->getTypeId()),
