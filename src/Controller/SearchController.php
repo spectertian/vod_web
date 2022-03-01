a102e04fd0817a53a6239233f0c71c5365264cfc
zhongsheng <tianzhongsheng168@gmail.com>
2022-03-01 22:32:42 +0800

    兼容手机

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -9,17 +9,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Mobile_Detect;
 
 class SearchController extends AbstractController
 {
     #[Route('/search.html', name: 'search', options: ['sitemap' => true])]
-    public function index(VodListRepository $vodListRepository, Request $request, PaginatorInterface $paginator, RecommendList $recommendList): Response
+    public function index(VodListRepository $vodListRepository, Request $request, PaginatorInterface $paginator, RecommendList $recommendList, Mobile_Detect $mobile_Detect): Response
     {
-
         $topicList = $recommendList->getTopic(8);
+        $keyword   = $request->get("wd");
+        $query     = $vodListRepository->createQueryBuilder();
 
-        $keyword = $request->get("wd");
-        $query   = $vodListRepository->createQueryBuilder();
         if ($keyword != '') {
             $query->addOr($query->expr()->field('director')->in([new \MongoDB\BSON\Regex($keyword, "i")]));
             $query->addOr($query->expr()->field('stars')->in([new \MongoDB\BSON\Regex($keyword, "i")]));
@@ -29,17 +29,38 @@ class SearchController extends AbstractController
         $query->sort('vod_time', 'DESC');
 
         $pagination = $paginator->paginate(
-            $query, /* query NOT result */
+            $query,
             $request->query->getInt('page', 1), 10);
 
         $pagination->setParam('wd', $keyword);
-        $pagination->setTemplate('show/pagination.html.twig');
 
-        return $this->render('search/index.html.twig', [
+
+        if ($mobile_Detect->isMobile()) {
+            $tmp   = 'm/search/index.html.twig';
+            $tmpPg = 'm/show/pagination.html.twig';
+        } else {
+            $tmp   = 'search/index.html.twig';
+            $tmpPg = 'show/pagination.html.twig';
+        }
+
+        $pagination->setTemplate($tmpPg);
+
+        return $this->render($tmp, [
             'pagination' => $pagination,
             'title'      => '搜索结果',
             'wd'         => $keyword,
             'topicList'  => $topicList,
+        ]);
+    }
+
+    #[Route('/search_m.html', name: 'search_m')]
+    public function m(VodListRepository $vodListRepository, Request $request, PaginatorInterface $paginator, RecommendList $recommendList, Mobile_Detect $mobile_Detect): Response
+    {
+        $topicList = $recommendList->getTopic(8);
+
+        return $this->render('m/search/m.html.twig', [
+            'title'     => '搜索结果',
+            'topicList' => $topicList,
         ]);
     }
 }
